0df025a945a89136390ac740096dbbdf0d5a6434
KoSangWon <jackoss@naver.com>
2020-02-29 01:30:01 +0900

    Getting the weather

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,19 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const API_KEY = "9f7e9d1141462d0344352a2b83d01dc5";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  };
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+      error => {
+        this.setState({
+          error:error
+        });
+      }
+    );
+  }
+
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature:json.main.temp,
+        name:json.weather[0].main,
+        isLoaded:true
+      })
+      ;
+    });
+  };
+
+  render() {
+    const {isLoaded, error } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true}/>
+        {isLoaded ? <Weather/> : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  errorText:{
+    color:"red",
+    backgroundColor:"transparent",
+    marginBottom:40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100,
+  }
 });
