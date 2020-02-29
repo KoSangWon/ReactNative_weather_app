95150aeed7d81e01e247dc4d92d03cad113da0d7
KoSangWon <jackoss@naver.com>
2020-02-29 16:31:25 +0900

    Changing the icons

--- weather-app/Weather.js
+++ weather-app/Weather.js
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
+const weatherCases = {
+    Rain:{
+        colors:["#00C6FB", "#005BEA"],
+        title: "Raining",
+        subtitle: "For more info look outside",
+        icon: "weather-rainy"
+    },
+    Clear:{
+        colors:["#FEF253", "#FF7300"],
+        title: "Sunny",
+        subtitle: "Sunny day~~",
+        icon: "weather-sunny"
+    },
+    Thunderstorm:{
+        colors:["#00ECBC", "#007ADF"],
+        title: "Thunderstorm in the house",
+        subtitle: "Actually, outside of the house",
+        icon: "weather-lightening"
+    },
+    Clouds:{
+        colors:["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "I know, it is boring",
+        icon: "weather-cloudy"
+    },
+    Snow:{
+        colors:["#7DE2FC", "#8986E5"],
+        title: "Cold as balls",
+        subtitle: "Do you wanna build a snowman~",
+        icon: "weather-snowy"
+    },
+    Drizzle:{
+        colors:["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "It like rain",
+        icon: "weather-hail"
+    },
+    Haze:{
+        colors:["#89F7FE", "#66A6FF"],
+        title: "Haze",
+        subtitle: "비도 오고 그래서~",
+        icon: "weather-windy"
+    },
+    Mist:{
+        colors:["#89F7FE", "#66A6FF"],
+        title: "Mist!!",
+        subtitle: "It's like you have no glasses on",
+        icon: "weather-fog"
+    }
+}
+
+/*
 export default class Weather extends Component{
     render() {
         return (
@@ -19,6 +72,30 @@ export default class Weather extends Component{
         );
     }
 }
+*/
+
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    weatherName: PropTypes.string.isRequired
+}
+
+function Weather({weatherName, temp}){
+    //console.log(weatherName);//날씨 확인
+    return (
+        <LinearGradient colors={weatherCases[weatherName].colors} style={styles.container}>
+            <View style={styles.upper}>
+                <MaterialCommunityIcons color="white" size={144} name={weatherCases[weatherName].icon} />
+                <Text style={styles.temp}>{temp}°C </Text>
+            </View>
+            <View style={styles.lower}>
+                <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+                <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>
+            </View>
+        </LinearGradient>
+    );
+}
+
+export default Weather;
 
 const styles = StyleSheet.create({
     container:{
